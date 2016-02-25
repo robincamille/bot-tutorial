88b7e37f6709a3f2a96bffc4d0f8494be6308288
rdvs18@gmail.com <root@libcircmac.jjay.cuny.edu>
2016-02-25 15:46:25 -0500

    Improved madlibs bot; delete madlibs folder

--- mashup_madlib.py
+++ mashup_madlib.py
@@ -10,7 +10,7 @@
 # Housekeeping: do not edit
 import json, io, tweepy, time, urllib2
 from random import randint
-from credentials import *
+from credentials_lacunybot import *
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
@@ -19,31 +19,36 @@ api = tweepy.API(auth)
 # Housekeeping: opening JSON files
 # Find more item lists at https://github.com/dariusk/corpora/tree/master/data
 # Click "Raw" button, copy URL
-objectfile = urllib2.urlopen('https://raw.githubusercontent.com/dariusk/corpora/master/data/objects/objects.json')
-objectlist = objectfile.read()
+list1file = urllib2.urlopen('https://raw.githubusercontent.com/dariusk/corpora/master/data/objects/objects.json')
+list1read = list1file.read()
 
-foodsfile = urllib2.urlopen('https://raw.githubusercontent.com/dariusk/corpora/master/data/foods/menuItems.json')
-foodslist = foodsfile.read()
+list2file = urllib2.urlopen('https://raw.githubusercontent.com/dariusk/corpora/master/data/foods/menuItems.json')
+list2read = list2file.read()
+
+list3file = urllib2.urlopen('https://raw.githubusercontent.com/dariusk/corpora/master/data/humans/occupations.json')
+list3read = list3file.read()
 
 # Create Python-readable lists of items in JSON files
-objs = json.loads(objectlist)['objects']
-foods = json.loads(foodslist)['menuItems']
+list1 = json.loads(list1read)['objects'] # Change 'objects' to the title of the list
+list2 = json.loads(list2read)['menuItems']
+list3 = json.loads(list3read)['occupations']
 
 
 # Repeatable poem-generator
 poemlist = []
 counter = 0
 
-while counter < 2: # Change 2 to however many poems you want to produce
+while counter < 1: # Change 2 to however many poems you want to produce
     
     # Pick random numbers
-    objnum = randint(0, len(objs) - 1)
-    foodnum = randint(0, len(foods) - 1)
+    list1num = randint(0, len(list1) - 1)
+    list2num = randint(0, len(list2) - 1)
+    list3num = randint(0, len(list3) - 1)
 
     # Choose random items from each list using random numbers
-    first = objs[objnum] # Syntax: list[number]
-    second = foods[foodnum].lower()
-    third = objs[objnum + 1] + 's'
+    first = list1[list1num] # Syntax: list[number]
+    second = list2[list2num].lower()
+    third = list3[list3num].lower() + 's'
 
     # Fill in the blanks of the poem
     poem = 'so much depends\nupon\n\na\n%s\n\nglazed with\n%s\n\nbeside the\n%s\n\n' \
